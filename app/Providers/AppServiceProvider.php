99db74913eef069ee5da8259620199d3d1ab1a77
adimertaaa <adimerta@student.unud.ac.id>
2021-10-12 17:23:49 +0800

    gas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        PenjualanSparepart::observe(PenjualanSparepartObserver::class);
-        PenerimaanService::observe(PenerimaanServiceObserver::class);
+       
     }
 }
