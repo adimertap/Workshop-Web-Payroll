ca35d578144d9c75453877ef3348fd99c8088e14
adimertaaa <adimerta@student.unud.ac.id>
2021-10-13 22:52:38 +0800

    gas

--- resources/views/pages/payroll/perampungan/edit.blade.php
+++ resources/views/pages/payroll/perampungan/edit.blade.php
@@ -23,8 +23,7 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-header">Basic Card Example</div>
-            <div class="card-body">
-                <h6>Enter your account information</h6>
+            <div class="card-body ml-2">
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group row">
@@ -92,12 +91,18 @@
                         </div>
                     </div>
                 </div>
-                <h6 class="mt-2">A. IDENTITAS PENERIMA PENGHASIL YANG DIPOTONG</h6>
+
+                {{-- BAGIAN A IDENTITAS ---------------------------------------------------------------------- --}}
+                <hr class="mt-2">
+                <h6>A. IDENTITAS PENERIMA PENGHASIL YANG DIPOTONG</h6>
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="npwp_terpotong" class="col-sm-2 col-form-label col-form-label-sm">1. NPWP</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="npwp_terpotong"
                                         value="{{ $perampungan->Pegawai->npwp_pegawai }}" readonly>
                                 </div>
@@ -106,7 +111,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="status_pegawai" class="col-sm-3 col-form-label col-form-label-sm">6. Status/Jumlah Tanggungan Keluarga</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="status_pegawai"
                                         value="{{ Auth::user()->pegawai->nama_pegawai }}" readonly>
                                 </div>
@@ -117,7 +125,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="nik_terpotong" class="col-sm-2 col-form-label col-form-label-sm">2. NIK/No Paspor</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="nik_terpotong"
                                         value="{{ $perampungan->Pegawai->nik_pegawai }}" readonly>
                                 </div>
@@ -126,7 +137,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="nama_jabatan" class="col-sm-3 col-form-label col-form-label-sm">7. Nama Jabatan</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="nama_jabatan"
                                         value="{{ $perampungan->Pegawai->Jabatan->nama_jabatan }}" readonly>
                                 </div>
@@ -137,7 +151,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="nama_pegawai" class="col-sm-2 col-form-label col-form-label-sm">3. Nama</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="nama_pegawai"
                                         value="{{ $perampungan->Pegawai->nama_pegawai }}" readonly>
                                 </div>
@@ -146,7 +163,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="karyawan_asing" class="col-sm-3 col-form-label col-form-label-sm">8. Karyawan Asing</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="karyawan_asing"
                                         value="{{ $perampungan->karyawan_asing }}" readonly>
                                 </div>
@@ -157,7 +177,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="alamat_pegawai" class="col-sm-2 col-form-label col-form-label-sm">4. Alamat</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="alamat_pegawai"
                                         value="{{ $perampungan->Pegawai->alamat }}" readonly>
                                 </div>
@@ -166,7 +189,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="kode_negara" class="col-sm-3 col-form-label col-form-label-sm">9. Kode Negara Domisili</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="kode_negara"
                                         value="{{ $perampungan->kode_negara }}" readonly>
                                 </div>
@@ -177,7 +203,10 @@
                         <div class="col-sm-6">
                             <div class="form-group row">
                                 <label for="jenis_kelamin" class="col-sm-2 col-form-label col-form-label-sm">5. Jenis Kelamin</label>
-                                <div class="col-sm-8">
+                                <div class="col-sm-1 text-center">
+                                    <span> : </span>
+                                </div>
+                                <div class="col-sm-6">
                                     <input type="input" class="form-control form-control-sm" id="jenis_kelamin"
                                         value="{{ $perampungan->Pegawai->jenis_kelamin }}" readonly>
                                 </div>
