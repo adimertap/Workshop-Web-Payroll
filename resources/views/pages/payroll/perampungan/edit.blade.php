060abd5c40f7d1f5317eccde86efd14458031b18
adimertaaa <adimerta@student.unud.ac.id>
2021-10-13 22:39:05 +0800

    gas

--- resources/views/pages/payroll/perampungan/edit.blade.php
+++ resources/views/pages/payroll/perampungan/edit.blade.php
@@ -49,7 +49,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class=" col-sm-6">
+                    <div class=" col-sm-5">
                         <div class="form-group row">
                             <label for="nomor" class="col-sm-3 col-form-label col-form-label-sm">Masa Perolehan
                                 Penghasilan</label>
@@ -64,7 +64,7 @@
                                 <input type="input" class="form-control form-control-sm" id="nomor"
                                     value="{{ date('m', strtotime($perampungan->masa_perolehan_akhir)) }}" readonly>
                             </div>
-                            <label for="nomor" class="col-sm-2 text-center  col-form-label col-form-label-sm">Tahun</label>
+                            <label for="nomor" class="col-sm-1 text-center  col-form-label col-form-label-sm">Th.</label>
                             <div class="col-sm-2">
                                 <input type="input" class="form-control form-control-sm" id="nomor"
                                     value="{{ date('Y', strtotime($perampungan->masa_perolehan_awal)) }}" readonly>
@@ -72,10 +72,11 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group row">
-                            <label for="id_pemotong" class="col-sm-3 col-form-label col-form-label-sm">Nama Pemotong</label>
+                            <label for="id_pemotong" class="col-sm-2 col-form-label col-form-label-sm">Nama Pemotong</label>
                             <div class="col-sm-8">
                                 <input type="input" class="form-control form-control-sm" id="id_pemotong"
                                     value="{{ Auth::user()->pegawai->nama_pegawai }}" readonly>
@@ -96,75 +97,7 @@
 
             </div>
         </div>
-
     </div>
-
-    {{-- <div class="container-fluid">
-        <div class="card">
-            <div class="card-header border-bottom">
-                <div class="nav nav-pills nav-justified flex-column flex-xl-row nav-wizard" id="cardTab" role="tablist">
-                    <a class="nav-item nav-link active" id="wizard1-tab" href="#wizard1" data-toggle="tab" role="tab"
-                        aria-controls="wizard1" aria-selected="true">
-                        <div class="wizard-step-icon">1</div>
-                        <div class="wizard-step-text">
-                            <div class="wizard-step-text-name">Identitas</div>
-                            <div class="wizard-step-text-details">Penerima Penghasil yang dipotong</div>
-                        </div>
-                    </a>
-                    <a class="nav-item nav-link" id="wizard2-tab" href="#wizard2" data-toggle="tab" role="tab"
-                        aria-controls="wizard2" aria-selected="false">
-                        <div class="wizard-step-icon">2</div>
-                        <div class="wizard-step-text">
-                            <div class="wizard-step-text-name">Rincian</div>
-                            <div class="wizard-step-text-details">Penghasilan dan Penghitungan PPh Pasal 21</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-
-            <div class="card-body">
-                <div class="tab-content" id="cardTabContent">
-                    <div class="tab-pane py-5 py-xl-10 fade active show" id="wizard1" role="tabpanel"
-                        aria-labelledby="wizard1-tab">
-                        <div class="row justify-content-center">
-                            <div class="col-xxl-6 col-xl-10">
-                                <h5 class="card-title">Enter your account information</h5>
-                                <form>
-                                  
-
-
-
-                                    <hr class="my-4">
-                                    <div class="d-flex justify-content-between">
-                                        <button class="btn btn-light disabled" type="button"
-                                            disabled="">Previous</button>
-                                        <button class="btn btn-primary" type="button">Next</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="tab-pane py-5 py-xl-10 fade" id="wizard2" role="tabpanel" aria-labelledby="wizard2-tab">
-                        <div class="row justify-content-center">
-                            <div class="col">
-                                <h3 class="text-primary">Step 2</h3>
-                                <h5 class="card-title">Enter your billing details</h5>
-                                <form>
-
-                                    <hr class="my-4">
-                                    <div class="d-flex justify-content-between">
-                                        <button class="btn btn-light" type="button">Previous</button>
-                                        <button class="btn btn-primary" type="button">Next</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
 </main>
 
 
