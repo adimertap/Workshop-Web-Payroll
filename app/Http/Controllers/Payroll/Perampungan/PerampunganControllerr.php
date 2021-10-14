826919330ca8d5f2bce16c7b5a1199a7cbcce91e
adimertaaa <adimerta@student.unud.ac.id>
2021-10-14 17:51:27 +0800

    ss

--- app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
+++ app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
@@ -108,6 +108,15 @@ class PerampunganControllerr extends Controller
         $perampungan = Perampungan::with('Pegawai','Pegawai.Jabatan','Pegawai.PTKP','Pemotong','Detail')->find($id);
 
         $detailgaji = Detailgaji::with('Gaji')->where('id_pegawai', $perampungan->id_pegawai)->get();
+        
+        for($i = 0;  $i < count($detailgaji->Gaji); $i++ ){
+            for($j = 0;  $j < count($detailgaji); $j++ ){
+               if ($detailgaji->Gaji[$i]->id_gaji_pegawai == $detailgaji[$j]->id_gaji_pegawai ){
+                $detailgaji[$j]->masa_perolehan = $detailgaji->Gaji[$i]->bulan_gaji;
+               };
+            }
+        }
+
         return $detailgaji;
 
 
