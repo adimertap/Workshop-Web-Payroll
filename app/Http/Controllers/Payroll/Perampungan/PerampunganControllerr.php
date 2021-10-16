4285f0e30a2b7414fa1bb99df1b7129c7ef12dc7
adimertaaa <adimerta@student.unud.ac.id>
2021-10-16 23:19:30 +0800

    gg

--- app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
+++ app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
@@ -148,7 +148,7 @@ class PerampunganControllerr extends Controller
         $detailgaji = Detailgaji::with([
             'Gaji'])
         ->join('tb_payroll_perhitungan_gaji', 'tb_payroll_detail_gaji.id_gaji_pegawai', 'tb_payroll_perhitungan_gaji.id_gaji_pegawai')
-        // ->where('id_pegawai', $tes->id_pegawai)
+        ->where('id_pegawai', $tes->id_pegawai)
         ->whereBetween('bulan_gaji', [$perampungan->masa_perolehan_awal, $perampungan->masa_perolehan_akhir])
         ->get();
 
