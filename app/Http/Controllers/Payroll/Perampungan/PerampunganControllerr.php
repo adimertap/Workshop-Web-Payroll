708dce84a948a9e2e4eaf8512a4a60aa4f855216
adimertaaa <adimerta@student.unud.ac.id>
2021-10-16 23:12:16 +0800

    gg

--- app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
+++ app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
@@ -140,7 +140,7 @@ class PerampunganControllerr extends Controller
     {
         $perampungan = Perampungan::with('Detail')->find($id);
         $id_pegawai = Perampungan::join('tb_payroll_detail_perampungan', 'tb_payroll_perampungan.id_perampungan', 'tb_payroll_detail_perampungan.id_perampungan')
-        ->where('id_perampungan', $id)->toArray();
+        ->where('id_perampungan', $id)->first();
 
         // return $id_pegawai;
 
