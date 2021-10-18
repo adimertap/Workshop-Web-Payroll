1259811c16a801c9b194104daf71e2c34eead817
adimertaaa <adimerta@student.unud.ac.id>
2021-10-18 12:50:22 +0800

    gg

--- resources/views/pages/payroll/perampungan/edit.blade.php
+++ resources/views/pages/payroll/perampungan/edit.blade.php
@@ -133,12 +133,10 @@
                                 <div class="form-group row">
                                     <label for="nomor" class="col-sm-2 col-form-label col-form-label-sm">Nomor</label>
                                     <div class="col-sm-8">
-                                        @forelse ($kode as $kodes)
+                                      
                                         <input type="input" class="form-control form-control-sm" id="nomor" value="{{ $kodes->id_1721a1 }}"
                                             readonly>
-                                        @empty
-                                            
-                                        @endforelse
+                        
                                        
                                     </div>
                                 </div>
