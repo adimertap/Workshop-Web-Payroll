205b930bd29008231465a42a28f9e7ea2928f1e8
adimertaaa <adimerta@student.unud.ac.id>
2021-10-18 12:18:44 +0800

    gg

--- app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
+++ app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
@@ -127,11 +127,12 @@ class PerampunganControllerr extends Controller
         $gji = Detailgaji::leftJoin('tb_payroll_perhitungan_gaji', 'tb_payroll_detail_gaji.id_gaji_pegawai', 'tb_payroll_perhitungan_gaji.id_gaji_pegawai')
         ->join('tb_payroll_detail_perampungan', 'tb_payroll_detail_gaji.id_pegawai', 'tb_payroll_detail_perampungan.id_pegawai')
         ->select('tb_payroll_detail_perampungan.id_pegawai', 'id_1721a1')
-        ->whereIn('tb_payroll_detail_perampungan.id_pegawai', $tes)->groupBy('id_pegawai')
+        ->groupBy('tb_payroll_detail_perampungan.id_pegawai')
         ->whereBetween('bulan_gaji', [$perampungan->masa_perolehan_awal, $perampungan->masa_perolehan_akhir])
         ->get();
 
         return $gji;
+        
         
         
 
