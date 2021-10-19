d48d1633fb20891d7b32c3a82764f5ff5409549e
adimertaaa <adimerta@student.unud.ac.id>
2021-10-19 15:54:14 +0800

    gg

--- app/Http/Controllers/Payroll/Gajipegawai/GajipegawaiController.php
+++ app/Http/Controllers/Payroll/Gajipegawai/GajipegawaiController.php
@@ -207,7 +207,7 @@ class GajipegawaiController extends Controller
         ->find($id_gaji_pegawai);
         
         $tes = Detailgaji::join('tb_payroll_detail_tunjangan','tb_payroll_detail_gaji.id_pegawai','tb_payroll_detail_tunjangan.id_pegawai')
-        ->where('tb_payroll_detail_gaji.id_gaji_pegawai', $id_gaji_pegawai)
+        ->where('tb_payroll_detail_tunjangan.id_pegawai','tb_payroll_detail_gaji.id_pegawai' )
         ->get();
 
         return $tes;
