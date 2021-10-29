cfe81f070a6ede4f4824066a80d48765c95d5b71
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-29 10:45:24 +0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-    public const SSO = '/sso';
+    public const SSO = '/payroll/gaji-pegawai';
 
     /**
      * Define your route model bindings, pattern filters, etc.
