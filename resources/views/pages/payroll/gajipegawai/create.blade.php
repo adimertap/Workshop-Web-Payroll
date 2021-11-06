5563741f66ed9ec6f0aa00fb84b488626a7d6df4
adimertaaa <adimerta@student.unud.ac.id>
2021-11-07 00:08:30 +0800

    gg

--- resources/views/pages/payroll/gajipegawai/create.blade.php
+++ resources/views/pages/payroll/gajipegawai/create.blade.php
@@ -974,6 +974,9 @@
                 text: 'Anda Belum Memilih Pegawai',
             })
         } else {
+            var sweet_loader =
+                '<div class="sweet_loader"><svg viewBox="0 0 140 140" width="140" height="140"><g class="outline"><path d="m 70 28 a 1 1 0 0 0 0 84 a 1 1 0 0 0 0 -84" stroke="rgba(0,0,0,0.1)" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></g><g class="circle"><path d="m 70 28 a 1 1 0 0 0 0 84 a 1 1 0 0 0 0 -84" stroke="#71BBFF" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-dashoffset="200" stroke-dasharray="300"></path></g></svg></div>';
+                
             var data = {
                 _token: _token,
                 grand_total_gaji: grand_total_gaji,
