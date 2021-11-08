d719c5ec9526950601eabfba27715fc6c937bf33
adimertaaa <adimerta@student.unud.ac.id>
2021-11-08 17:43:30 +0800

    gg

--- app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
+++ app/Http/Controllers/Payroll/Perampungan/PerampunganControllerr.php
@@ -114,10 +114,11 @@ class PerampunganControllerr extends Controller
         ->whereBetween('bulan_gaji', [$perampungan->masa_perolehan_awal, $perampungan->masa_perolehan_akhir])
         ->get();
 
+        return $detailgaji;
+
         $gas = DetailPerampungan::join('tb_kepeg_master_pegawai','tb_payroll_detail_perampungan.id_pegawai','tb_kepeg_master_pegawai.id_pegawai')->where('id_perampungan', $perampungan->id_perampungan)
         ->get();
 
-        return $gas;
 
         $blt = date('m');
         $year = date('y');
